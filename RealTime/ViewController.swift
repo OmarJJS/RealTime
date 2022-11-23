8c8cfd77a42dabf0f697cee6f2ee63cbb75450ab
Omar Jimenez <jojimenezs98@gmail.com>
2022-11-22 18:33:57 -0600

    Initial Commit

---
+++ RealTime/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealTime
+//
+//  Created by MacBookMBP1 on 22/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
